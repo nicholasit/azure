65e6814b315a1bd5a3db5689db61253afda78aba
Nicholas Marshall Micaloski <nicholas.micaloski@gmail.com>
2020-04-30 15:09:52 -0300

    Update RDS_Azure_Preparation_Tenant_WindowsVirtualDesktop.ps1

--- RDS_Azure_Preparation_Tenant_WindowsVirtualDesktop.ps1
+++ RDS_Azure_Preparation_Tenant_WindowsVirtualDesktop.ps1
@@ -1,3 +1,8 @@
+Install-Module -Name Microsoft.RDInfra.RDPowerShell -Force
+Import-Module -Name Microsoft.RDInfra.RDPowerShell
+Install-Module -Name Az -AllowClobber -Force
+Import-Module -Name Az
+
 #Steps before start wizard for Windows Virtual Desktop
 New-RdsTenant -Name AZR_RDS -AadTenantId XXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX -AzureSubscriptionId XXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX
 New-RdsRoleAssignment -TenantName AZR_RDS_FIBRA -SignInName admin@tenant.onmicrosoft.com -RoleDefinitionName "RDS Owner"

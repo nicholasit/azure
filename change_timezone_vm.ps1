37b48313d745c04c8b18f53d0c60dbc6cb6733c0
Nicholas Marshall Micaloski <nicholas.micaloski@gmail.com>
2023-07-13 16:40:00 -0300

    Update change_timezone_vm.ps1

--- change_timezone_vm.ps1
+++ change_timezone_vm.ps1
@@ -1,3 +1,6 @@
 Get-TimeZone -ListAvailable
 Get-TimeZone -ListAvailable | where ({$_.Id -like "Pacific*"})
 Set-TimeZone -Id "Pacific Standard Time"
+
+## Para brasil usar:
+Set-TimeZone -Id "E. South America Standard Time"

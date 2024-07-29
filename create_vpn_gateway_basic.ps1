5dc356d6a4e8d8eb73004294567a6b5a2d306d23
Nicholas Marshall Micaloski <nicholas.micaloski@gmail.com>
2024-07-29 08:57:29 -0300

    Create create_vpn_gateway_basic.ps1

---
+++ create_vpn_gateway_basic.ps1
@@ -0,0 +1,11 @@
+﻿$location = 'eastus'
+$resourceGroup = 'rg-vnethub-marshalls-prod-eastus'
+$vnetName = 'vnethub-marshalls-prod-eastus-001'
+$publicipName = 'pip-vgw-marshalls-prod-southbr-001'
+$gatewayName = 'vgw-marshalls-prod-eastus-001'
+
+$vnet = Get-AzVirtualNetwork -ResourceGroupName $resourceGroup -Name $vnetName
+$subnet = Get-AzVirtualNetworkSubnetConfig -Name 'GatewaySubnet' -VirtualNetwork $vnet
+$publicip = New-AzPublicIpAddress -Name $publicipName -ResourceGroupName $resourceGroup -Location $location -Sku Basic -AllocationMethod Dynamic
+$ipconfig = New-AzVirtualNetworkGatewayIpConfig -Name 'GWIPConfig-01' -SubnetId $subnet.Id -PublicIpAddressId $publicip.Id
+New-AzVirtualNetworkGateway -Name $gatewayName -ResourceGroupName $resourceGroup -Location $location -IpConfigurations $ipconfig -GatewayType 'VPN' -VpnType 'RouteBased' -GatewaySku 'Basic'

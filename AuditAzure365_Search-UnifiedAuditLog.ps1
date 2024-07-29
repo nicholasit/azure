29c991dfac0573d647cb9fac3a52cc8a3d53df12
Nicholas Marshall Micaloski <nicholas.micaloski@gmail.com>
2024-07-29 08:54:01 -0300

    Create AuditAzure365_Search-UnifiedAuditLog.ps1

---
+++ AuditAzure365_Search-UnifiedAuditLog.ps1
@@ -0,0 +1,10 @@
+﻿$OutputFile = "c:\temp\log_30days.csv"
+$Today = Get-Date -Date (Get-Date -Format “yyyy-MM-dd”)
+$intDays = 30
+For ($i=0; $i -le $intDays; $i++){
+  For ($j=23; $j -ge 0; $j--){
+    $StartDate = ($Today.AddDays(-$i)).AddHours($j)
+    $EndDate = ($Today.AddDays(-$i)).AddHours($j + 1)
+    $Audit = Search-UnifiedAuditLog -StartDate $StartDate -EndDate $EndDate
+    $Audit | Export-Csv $OutputFile -NoTypeInformation -Append
+    Write-Host $StartDate `t $Audit.Count}}

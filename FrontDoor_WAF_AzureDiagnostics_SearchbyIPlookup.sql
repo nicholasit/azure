198b1ad8d7305301d22372d157a357e4a48b36dd
Nicholas Marshall Micaloski <nicholas.micaloski@gmail.com>
2024-08-01 16:01:38 -0300

    Add Front Door Standard Premium
    Add Front Door Standard Premium

--- FrontDoor_WAF_AzureDiagnostics_SearchbyIPlookup.sql
+++ FrontDoor_WAF_AzureDiagnostics_SearchbyIPlookup.sql
@@ -1,6 +1,18 @@
+### AZURE FRONT DOOR CLASSIC ###
+
 AzureDiagnostics
 | where ResourceProvider == "MICROSOFT.NETWORK" and Category == "FrontdoorWebApplicationFirewallLog"
 | where action_s == "Block"
 | where clientIP_s == "IP_HERE"
 | summarize RequestCount = count() by ClientIP = clientIP_s, UserAgent = userAgent_s, RuleName = ruleName_s ,Resource
+| order by RequestCount desc
+
+
+
+### AZURE FRONT DOOR STANDARD/PREMIUM ###
+
+| where ResourceProvider == "MICROSOFT.CDN" and Category == "FrontDoorWebApplicationFirewallLog"
+| where action_s == "Block"
+| where clientIP_s == "IP_HERE"
+| summarize RequestCount = count() by ClientIP = clientIP_s, UserAgent = userAgent_s, RuleName = ruleName_s,Resource
 | order by RequestCount desc
